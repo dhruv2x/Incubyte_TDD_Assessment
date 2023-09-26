508f650f777c540ef461356c526fef92d0ba91d4
Dhruv <chauhandhruv351@gmail.com>
2023-09-26 22:12:31 +0530

    Add failing test for complex movement

--- test/spacecraftController.test.js
+++ test/spacecraftController.test.js
@@ -14,3 +14,19 @@ test("If only one command", () => {
     const output = mainCommand(commands, start);
     expect(output).toStrictEqual(expected);
   });
+
+  test("Complex Movement", () => {
+    const commands = ["f", "r", "u", "b", "l"];
+  
+    const start = {
+      coordinates: [0, 0, 0],
+      direction: "N",
+    };
+    const expected = {
+      coordinates: [0, 1, -1],
+      direction: "N",
+    };
+  
+    const output = mainCommand(commands, start);
+    expect(output).toStrictEqual(expected);
+  });
