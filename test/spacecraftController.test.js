3fc8c761c24b926ad5923d9cb50ea4e7716c062e
Dhruv <chauhandhruv351@gmail.com>
2023-09-26 22:25:57 +0530

    Add 4 failing test for complex movements

--- test/spacecraftController.test.js
+++ test/spacecraftController.test.js
@@ -1,3 +1,4 @@
+const spacecraftController = require("../modules/spacecraftController").spacecraftController;
 test("If only one command", () => {
     const commands = ["b"];
   
@@ -41,6 +42,70 @@ test("If only one command", () => {
     const expected = {
       coordinates: [0, 0, 5],
       direction: "U",
+    };
+  
+    const output = mainCommand(commands, start);
+    expect(output).toStrictEqual(expected);
+  });
+
+  test("Complex Movement", () => {
+    const commands = ["f", "f", "u", "l", "l"];
+  
+    const start = {
+      coordinates: [0, 0, 0],
+      direction: "W",
+    };
+    const expected = {
+      coordinates: [-2, 0, 0],
+      direction: "W",
+    };
+  
+    const output = mainCommand(commands, start);
+    expect(output).toStrictEqual(expected);
+  });
+  
+  test("Complex Movement", () => {
+    const commands = ["f", "r", "u", "b", "r"];
+  
+    const start = {
+      coordinates: [0, 0, 0],
+      direction: "U",
+    };
+    const expected = {
+      coordinates: [0, 0, 0],
+      direction: "S",
+    };
+  
+    const output = mainCommand(commands, start);
+    expect(output).toStrictEqual(expected);
+  });
+  
+  test("Complex Movement", () => {
+    const commands = ["f", "r", "b", "b", "l"];
+  
+    const start = {
+      coordinates: [0, 0, 0],
+      direction: "N",
+    };
+    const expected = {
+      coordinates: [-2, 1, 0],
+      direction: "N",
+    };
+  
+    const output = mainCommand(commands, start);
+    expect(output).toStrictEqual(expected);
+  });
+  
+  test("Complex Movement", () => {
+    const commands = ["f", "r", "d"];
+  
+    const start = {
+      coordinates: [0, 0, 0],
+      direction: "N",
+    };
+    const expected = {
+      coordinates: [0, 1, 0],
+      direction: "D",
     };
   
     const output = mainCommand(commands, start);
